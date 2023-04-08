9462adbd87e2b758b58c29ab9994e91637a28341
rishabhj247 <rishabh.j@chapter247.com>
2023-04-08 17:50:16 +0530

    build main file from scratch

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int day = 28;
+    return MaterialApp(
+      home: Material(child: Center(child: Text("Welcome to $day days"))),
+    );
+  }
+}
