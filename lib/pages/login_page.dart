a11e23c194bdd12ccf7b158c9bd71f2d76c5852b
rishabhj247 <rishabh.j@chapter247.com>
2023-04-09 13:54:58 +0530

    custom animation with container and async await

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:learndart/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -14,9 +21,9 @@ class LoginPage extends StatelessWidget {
               "assets/images/login_img.png",
               fit: BoxFit.cover,
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            Text(
+              "Welcome $name",
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
             Padding(
@@ -25,6 +32,10 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                     decoration: const InputDecoration(
                         hintText: "Enter username", labelText: "Username"),
                   ),
@@ -34,13 +45,45 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter password", labelText: "Password"),
                   ),
                   const SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
+                      if (context.mounted) {
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(150, 50)),
-                      child: const Text("Login"))
+                      }
+                    },
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: changeButton
+                              ? BorderRadius.circular(50)
+                              : BorderRadius.circular(8)),
+                      child: changeButton
+                          ? const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : const Text("Login",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                    ),
+                  )
+                  // ElevatedButton does not have extra animation effect in it
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //     },
+                  //     style: TextButton.styleFrom(
+                  //         minimumSize: const Size(150, 50)),
+                  //     child: const Text("Login"))
                 ],
               ),
             )
